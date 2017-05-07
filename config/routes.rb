92f66f28d861e8e3e0efea3cd3a7a39582c27bcb
JustusFT <JustusFT@gmail.com>
2017-05-07 11:01:16 +0800

    solved the user controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   resources :welcome, only: [:index]
+  resources :users, only: [:new, :edit, :create, :update, :destroy]
+  resources :statuses, only: [:index]
 
 end
