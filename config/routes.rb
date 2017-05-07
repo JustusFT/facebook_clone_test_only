0a440c23d40326f1409c46426313e36f3e66fc89
JustusFT <JustusFT@gmail.com>
2017-05-07 11:32:42 +0800

    solved session controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   resources :welcome, only: [:index]
   resources :users, only: [:new, :edit, :create, :update, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :statuses, only: [:index]
 
+  root "welcome#index"
+
+  get '/signin', to:'sessions#new'
+  delete '/signout', to: 'sessions#destroy'
 end
