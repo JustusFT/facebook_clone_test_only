b7f461c05a5968f96d883dd8fb4cd5c2ea1b3220
JustusFT <JustusFT@gmail.com>
2017-05-07 14:29:34 +0800

    added specs for session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+  def logged_in?
+    !session[:user_id].nil?
+  end
+
+  def current_user
+    return nil unless logged_in?
+    User.find(session[:user_id])
+  end
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,5 +11,30 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let(:user){ User.create(id: 1, name: "test", email:"test@test.test", password: "test", password_confirmation: "test") }
+
+  describe "#logged_in?" do
+    it "returns false if a user is not logged in" do
+      session[:user_id] = nil
+      expect(helper.logged_in?).to be(false)
+    end
+
+    it "returns true if a user is logged in" do
+      session[:user_id] = 1
+      expect(helper.logged_in?).to be(true)
+    end
+  end
+
+  describe "current_user" do
+    it "returns a user" do
+      user = FactoryGirl.create(:user)
+      session[:user_id] = user.id
+      expect(current_user).to eq(user)
+    end
+
+    it "returns nil if no user is present" do
+      session[:user_id] = nil
+      expect(current_user).to be(nil)
+    end
+  end
 end
